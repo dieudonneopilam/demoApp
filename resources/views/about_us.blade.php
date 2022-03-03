0e5469d25529e12bd1b5990fde88f3f124dfd7b5
opilam <dieudonneopilam2@gmail.com>
2022-03-03 21:46:00 +0200

    add about page and update link

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -4,14 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>hello city</title>
+        <title>About-us | hello city</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body class="antialiased">
-        <h1>Hello from quebec</h1>
+        <p>Built with &hearts; by opilam</p>
+        <p><a href="/">Revenir a la page d'acceuil</a></p>
 
         <p>I'ts currently {{ date('h:i A') }}</p>
 
