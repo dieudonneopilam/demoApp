b00aa697d51e6d7d4f22d2a2da2d8622ebf93b29
opilam <dieudonneopilam2@gmail.com>
2022-03-05 20:43:22 +0200

    ajout des images avec la fonction asset

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -3,7 +3,7 @@
 @section('title','About-us | '. env('APP_NAME'))
 
 @section('content')
-        <img src="images/about.png" alt="about page">
+        <img src="{{ asset('/images/about.png')}}" alt="about page">
         <p>Built with &hearts; by opilam</p>
         <p><a href=" {{ route('home') }} ">Revenir a la page d'acceuil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title',config('app.name'))
 
 @section('content')
-        <img src="/images/home.png" alt="home page">
+        <img src=" {{ asset('/images/home.png')}} " alt="home page">
         <h1>Hello from quebec</h1>
 
         <p>I'ts currently {{ date('h:i A') }}</p>
